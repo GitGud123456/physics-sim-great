9c298fed163490fa59c6ad56e131671d2b729aa8
GitGud123456 <98549374+GitGud123456@users.noreply.github.com>
2023-06-09 16:03:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ let clearbtn = document.getElementById("clear");
 clearbtn.addEventListener("click", clear);
 document.addEventListener("keyup", initialize());
 document.addEventListener("keyup", bhinitialize());
+let parametersarray = [];
+document.addEventListener("keyup", applyParameters());
 btn.addEventListener("click", makeRandom, initialize);
 let lastTime = 0;
 let dt = 0.013213999999999998;
@@ -25,8 +27,8 @@ function returnclick(event) {
     document.getElementById("bx").value = x;
     var ax = 0;
     var ay = 0;
-    var Fnetx = 0;
-    var Fnety = 0;
+    var Fnetx;
+    var Fnety;
     objects.push(new Object(x, y, vx, vy, m, r, ax, ay, Fnetx, Fnety));
   } else if (select.value == "bh") {
     var [bhm, bhr, bhvx, bhvy] = bhinitialize();
@@ -40,14 +42,6 @@ function returnclick(event) {
 
 // motion stuff
 function update() {
-  [
-    parameterAttractionFroce,
-    parameterAirFriction,
-    parameterCollisonResponse,
-    parameterViscosity,
-    GravityCoeffcient,
-  ] = applyParameters();
-
   for (var i = 0; i < objects.length; i++) {
     objects[i].update();
     for (var j = i + 1; j < objects.length; j++) {
@@ -94,25 +88,52 @@ Object.prototype.update = function () {
   //safety
   if (this.x <= 0 + this.r) {
     this.x = 0 + this.r;
-    this.vx *= -1;
+    this.vx *= -0.9;
+    this.ax *= -0.9;
   }
   if (this.x >= canvas.width - this.r) {
     this.x = canvas.width - this.r;
-    this.vx *= -1;
+    this.vx *= -0.9;
+    this.ax *= -0.9;
   }
   if (this.y <= 0 + this.r) {
     this.y = 0 + this.r;
-    this.vy *= -1;
+    this.vy *= -0.9;
+    this.ay *= -0.9;
   }
   if (this.y >= canvas.height - this.r) {
     this.y = canvas.height - this.r;
-    this.vy *= -1;
+    this.vy *= -0.9;
+    this.ay *= -0.9;
   }
-  this.Fnetx = (this.vx + Ff(this.m, this.vx) + this.ax) * dt;
-  this.Fnety = (this.vy + Ff(this.m, this.vy) + this.ay) * dt;
-  console.log(this.x, this.vx, this.Fnetx);
-  this.x += this.Fnetx;
-  this.y += this.Fnety;
+  if (holearray.length >= 1) {
+    for (var bh = 0; bh < holearray.length; bh++) {
+      [G, Gx, Gy, D, dx, dy] = bGF(
+        this.x,
+        this.y,
+        this.m,
+        holearray[bh].m,
+        holearray[bh].x,
+        holearray[bh].y
+      );
+      console.log(Gx);
+      this.Fnetx = this.ax + Ff(this.m, this.vx) - Gx;
+      this.Fnety = this.ay + Ff(this.m, this.vy) + Gy;
+      this.vx += this.Fnetx * dt;
+      this.vy += this.Fnety * dt;
+
+      this.x += this.vx * dt;
+      this.y += this.vy * dt;
+    }
+  } else {
+    this.Fnetx = this.ax + Ff(this.m, this.vx);
+    this.Fnety = this.ay + Ff(this.m, this.vy);
+    this.vx += this.Fnetx * dt;
+    this.vy += this.Fnety * dt;
+
+    this.x += this.vx * dt;
+    this.y += this.vy * dt;
+  }
 };
 
 //animate objects
@@ -143,49 +164,35 @@ function animate() {
     objects[i].draw();
     //objects[i].update();
   }
-
-  update();
   requestAnimationFrame(time);
+  update();
+  parametersarray = applyParameters();
 }
-
 animate();
+//setInterval(animate, dt);
 
 //helper Functions
 
-function bGF(obj1, m, x, y) {
-  [
-    parameterAttractionFroce,
-    parameterAirFriction,
-    parameterCollisonResponse,
-    parameterViscosity,
-    GravityCoeffcient,
-  ] = applyParameters();
-
+function bGF(x1, y1, m1, m2, x2, y2) {
   var G =
-    (GravityCoeffcient * obj1.m * m) /
-    Math.sqrt((obj1.x - x) ** 2 + (obj1.y - y) ** 2) ** 2;
-  var D = Math.sqrt((obj1.x - x) ** 2 + (obj1.y - y) ** 2);
-  var dx = obj1.x - x;
-  var dy = obj1.y - y;
+    (parametersarray[0].GravityCoeffcient * m1 * m2) /
+    Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 2;
+  var D = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+  var dx = x1 - x2;
+  var dy = y1 - y2;
   var angle = Math.atan2(dx, dy);
-  var ay = Math.cos(angle) * G;
-  var ax = Math.sin(angle) * G;
-  if (G > 3) {
-    G = 3;
-  }
-  return [G, D, dx, dy, ax, ay];
+  var Gy = Math.sin(angle) * G;
+  var Gx = Math.cos(angle) * G;
+  // if (G > 3) {
+  //   G = 3;
+  // }
+  console.log(Gx);
+  return [G, Gx, Gy, D, dx, dy];
 }
 function GF(obj1, obj2) {
-  [
-    parameterAttractionFroce,
-    parameterAirFriction,
-    parameterCollisonResponse,
-    parameterViscosity,
-    GravityCoeffcient,
-  ] = applyParameters();
-  console.log([obj1, obj2]);
+  //console.log([obj1, obj2]);
   var G =
-    (GravityCoeffcient * obj1.m * obj2.m) /
+    (parametersarray[0].GravityCoeffcient * obj1.m * obj2.m) /
     Math.sqrt((obj1.x - obj2.x) ** 2 + (obj1.y - obj2.y) ** 2) ** 2;
   var D = Math.sqrt((obj1.x - obj2.x) ** 2 + (obj1.y - obj2.y) ** 2);
   var dx = obj1.x - obj2.x;
@@ -200,26 +207,12 @@ function areColliding(obj1, obj2) {
   var distance = Math.sqrt(dx ** 2 + dy ** 2);
   return distance <= obj1.r + obj2.r;
 }
-// function areColliding2(obj1, obj2) {
-//   var dx = obj1.x - obj2.x;
-//   var dy = obj1.y - obj2.y;
-//   var distance = Math.sqrt(dx ** 2 + dy ** 2);
-//   return distance <= obj1.r + obj2.r + 5;
-// }
 
 //calc Ff
 function Ff(m, Fnet) {
-  [
-    parameterAttractionFroce,
-    parameterAirFriction,
-    parameterCollisonResponse,
-    parameterViscosity,
-    GravityCoeffcient,
-  ] = applyParameters();
   var F = 0;
-
-  F = m * parameterAirFriction; // F coefficent x mass
-  if (Fnet > 0) {
+  F = m * parametersarray[0].parameterAirFriction; // F coefficent x mass
+  if (Fnet < 0) {
     F *= -1;
   }
   if (Math.abs(F) > Math.abs(Fnet)) {
@@ -228,7 +221,7 @@ function Ff(m, Fnet) {
   if (Fnet == 0) {
     F = 0;
   }
-
+  console.log(F);
   return F;
 }
 
@@ -243,7 +236,7 @@ function Object(x, y, vx, vy, m, r, ax, ay, Fnetx, Fnety) {
   this.ax = ax;
   this.ay = ay;
   this.Fnetx = Fnetx;
-  this.Fnety = Fnetx;
+  this.Fnety = Fnety;
 }
 //test objects
 
@@ -251,8 +244,8 @@ function Object(x, y, vx, vy, m, r, ax, ay, Fnetx, Fnety) {
 function makeRandom() {
   var x = Math.random() * canvas.width;
   var y = Math.random() * canvas.height;
-  var vx = Math.random() * 4;
-  var vy = Math.random() * 4;
+  var vx = Math.random() * 40;
+  var vy = Math.random() * 40;
   var m = Math.random() * 100;
   var r = m / 2;
   var ax = Math.random() * 2;
@@ -263,15 +256,17 @@ function makeRandom() {
 // objects.push(new Object(900, 325, -4, 0, 20, 10));
 
 //holearray.push(new gravityWell(500, 325, 0, 0, 10, 10));
-// for (var i = 0; i < 500; i++) {
-//   var x = Math.random() * canvas.width;
-//   var y = Math.random() * canvas.height;
-//   var vx = Math.random() * 4 - 2;
-//   var vy = Math.random() * 4 - 2;
-//   var m = Math.random() * 20;
-//   var r = m / 2;
-//   objects.push(new Object(x, y, vx, vy, m, r));
-// }
+for (var i = 0; i < 500; i++) {
+  var x = Math.random() * canvas.width;
+  var y = Math.random() * canvas.height;
+  var vx = Math.random() * 4 - 2;
+  var vy = Math.random() * 4 - 2;
+  var m = Math.random() * 20;
+  var r = m / 2;
+  var ax = Math.random() * 2;
+  var ay = Math.random() * 2;
+  objects.push(new Object(x, y, vx, vy, m, r, ax, ay));
+}
 
 // for (var i = 0; i < 1000; i++) {
 //   var x = Math.random() * canvas.width;
@@ -302,13 +297,17 @@ function applyParameters() {
   parameterCollisonResponse = +document.getElementById("pCr").value;
   parameterViscosity = +document.getElementById("pV").value;
   GravityCoeffcient = +document.getElementById("GC").value;
-  return [
-    parameterAttractionFroce,
-    parameterAirFriction,
-    parameterCollisonResponse,
-    parameterViscosity,
-    GravityCoeffcient,
+  var parameters = [
+    {
+      parameterAttractionFroce,
+      parameterAirFriction,
+      parameterCollisonResponse,
+      parameterViscosity,
+      GravityCoeffcient,
+    },
   ];
+  parametersarray = parameters;
+  return parameters;
 }
 
 //initalize ball values
